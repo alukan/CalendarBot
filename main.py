aa1f0f2ecf875460b5dcbd1e9da6ff1d0f5cd21b
alukan <kolyaterenya@gmail.com>
2024-02-07 18:08:54 +0100

    added /show

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ logging.basicConfig(filename=f'logs/{year}-{day}-{month}-main.log', level=loggin
 
 
 
-
 def check (e):
     return True
+
 
 
 async def runClient(client, token):
@@ -28,27 +28,34 @@ async def runClient(client, token):
 
     answer = "Hello, I am a bot, if you want to put new event type /add message YYYY-MM-DD HH:MM:SS"
     print("start")
-    messages = dict()
+    async def show_tasks(event):
+        for usr_id, task, date, type in messages:
+            if  event.peer_id.user_id == usr_id:
+                print("you have " + task + "has task on " + str(datetime.datetime.fromtimestamp(date)))
+                await event.reply("you have '" + task + "' task on " + str(datetime.datetime.fromtimestamp(date)))
+
 
     @client.on(events.NewMessage(incoming=True, func=check))
     async def handler(event):
         if (event.message.text[0:4] == "/add"):
             try:
-                name = ' '.join(event.message.text.split(" ")[1:-2])
-                print(name)
+                task = ' '.join(event.message.text.split(" ")[1:-2])
+                print(task)
                 date = event.message.text.split(" ")[-2:]
 
-                print("before")
                 datetime_object = datetime.datetime.strptime(' '.join(date), '%Y-%m-%d %H:%M:%S')
                 sec_date = datetime_object.timestamp()
-                print(sec_date)
                 hour_before = datetime.datetime.fromtimestamp(sec_date - 60 * 60)
-                print(hour_before)
-                await event.reply(name)
+                messages.append( (event.peer_id.user_id, task, sec_date, "event") ) 
+                await event.reply(task)
                 await event.reply(str(hour_before))  
             except Exception as exc:
                 print(exc)
                 await event.reply("wrong format") 
+        elif (event.message.text[0:5] == "/show"):
+            print("start show")
+            await show_tasks(event)
+            print("start2")
         else:
             print(event.message.text[0:4])
             await event.reply(answer)
@@ -66,6 +73,10 @@ async def main():
     id = data['API_ID']
     hash = data['API_HASH']
     token = data['TOKEN']
+    global enable
+    enable = data['logging']['enable'] == "true"
+    global messages
+    messages = []
     await asyncio.gather(runClient(TelegramClient('bot', id, hash), token))
 
 
